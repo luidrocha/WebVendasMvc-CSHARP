c76a60c00b172344f6baccd8197f85afd0b81ac7
Luidrocha <luidrocha@yahoo.com.br>
2022-01-03 16:59:06 -0300

    CRUD Scaffolding Department

---
+++ WebVendasMvc/WebVendasMvc/Data/WebVendasMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebVendasMvc.Models;
+
+namespace WebVendasMvc.Data
+{
+    public class WebVendasMvcContext : DbContext
+    {
+        public WebVendasMvcContext (DbContextOptions<WebVendasMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebVendasMvc.Models.Department> Department { get; set; }
+    }
+}
